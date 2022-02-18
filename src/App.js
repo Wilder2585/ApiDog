b529de389fd18a8f4e0c7f27bc71be449e19b42f
Wilder2585 <86340209+Wilder2585@users.noreply.github.com>
2022-02-18 00:20:30 -0500

    Usando una Api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Quote from "./components/Quote";
+import Spinner from "./components/Spinner";
+
+const inicialQuote = {
+
+  text: "",
+  author: ""
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+  const [quote, setQuote] = useState(inicialQuote);
+  const [loading, setLoading] = useState(false);  
+
+  const updateQuote = async()=>{
+    setLoading(true);
+    const url = "https://www.breakingbadapi.com/api/quote/random";
+    const res = await fetch(url);
+    const [newQuote] = await res.json(); //destructuramo con llaves, para que no aparesca en corchetes que es mas dificil manejar.
+    // hacer esto hace que la variable newQuote no imprima como array si no como un objeto.
+    
+      setQuote({
+        text: newQuote.quote,
+        author: newQuote.author
+      })
+      setLoading(false);
+  
+  }
+
+
+  useEffect (()=>{
+      updateQuote();  
+
+  },[])
+
+    return(
+      <div className="app">
+        
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+            alt="logo"
+          />
+          <button onClick={updateQuote}>Get another</button>
+         {
+          //esto es un operador terneario: El operador condicional (ternario) es el único operador en JavaScript que tiene tres operandos. Este operador se usa con frecuencia como atajo para la instrucción if.
+            loading ? <Spinner /> : <Quote quote={quote}/>
+
+         }
+
+
+      </div>
+    
   );
 }
 
